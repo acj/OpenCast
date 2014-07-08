cfe89da99f845bfc32b268efc357f5b1faf502b6
Adam Jensen <acj@linuxguy.org>
2014-07-08 19:38:03 -0400

    Initial framework skeleton

---
+++ OpenCast/OpenCast.h
@@ -0,0 +1,13 @@
+//
+//  OpenCast.h
+//  OpenCast
+//
+//  Created by Adam Jensen on 7/8/14.
+//  Copyright (c) 2014 Adam Jensen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenCast : NSObject
+
+@end
